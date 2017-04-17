6e1414d21940aae594684fd5c48518fd1b9ce3a9
Mathieuu97 <reviron.mathieu.33@gmail.com>
2017-04-17 23:55:26 +0200

    New class to implement

---
+++ source/MonoBot.java
@@ -0,0 +1,22 @@
+
+public class MonoBot implements Bot {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCommand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDecision() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

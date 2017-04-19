dd269d3486b5def0c19748ecc4b837c9784eeaaf
Mathieuu97 <reviron.mathieu.33@gmail.com>
2017-04-19 15:06:58 +0200

    MonoBot implementation

--- source/MonoBot.java
+++ source/MonoBot.java
@@ -1,16 +1,19 @@
 
 public class MonoBot implements Bot {
-
+	String TEAM_NAME = "MonoBot";
+	Player player =  new Player(TEAM_NAME, TEAM_NAME, 0);
+	Dice dice = new Dice();
+	Board board = new Board(dice);
+	
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return TEAM_NAME;
 	}
 
 	@Override
 	public String getCommand() {
-		// TODO Auto-generated method stub
-		return null;
+		String command = getDecision();
+		return command;
 	}
 
 	@Override

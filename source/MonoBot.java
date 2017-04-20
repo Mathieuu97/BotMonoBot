713d24eef1b291596beecebc356ac096a75220b6
Paul Kane <paul.kane@ucdconnect.ie>
2017-04-20 19:12:50 +0100

    Bot updated

--- source/MonoBot.java
+++ source/MonoBot.java
@@ -1,40 +1,55 @@
 
 public class MonoBot implements Bot {
+	
+	// The public API of YourTeamName must not change
+	// You cannot change any other classes
+	// YourTeamName may not alter the state of the board or the player objects
+	// It may only inspect the state of the board and the player objects
 	String TEAM_NAME = "MonoBot";
-	Player player =  new Player(TEAM_NAME, TEAM_NAME, 0);
 	Dice dice = new Dice();
 	Board board = new Board(dice);
+	Player player =  new Player(TEAM_NAME, TEAM_NAME, 0);
 	
-
-	public String getName() {
-		return TEAM_NAME;
-	}
-
-	public String getCommand() {
-		String command = getDecision();
-		return command;
+	MonoBot (BoardAPI board, PlayerAPI player, DiceAPI dice) {
+		return;
 	}
 	
-	private boolean hasEnoughMoney(){
-		if(player.getBalance()>=500) {
-			return true;
-		}
-		else {
-			return false;
-		}
+	public String getName () {
+		return "YourTeamName";
 	}
-
-	public String getDecision() {
-		if (board.getSquare(player.getPosition()) instanceof Property) {
+	
+	public String getCommand () {
+	
+		
+		return "roll"; 
+		
+	/*	if (board.getSquare(player.getPosition()) instanceof Property) {
 			Property property = (Property) board.getSquare(player.getPosition());
 			if (!property.isOwned()) {
 				if (hasEnoughMoney()) {
 					return "buy";
 				}
 			}
-			
-		}
+		} */
 		
-		return "done";
+		//return "done";  
 	}
+
+
+
+private boolean hasEnoughMoney(){
+	if(player.getBalance()>=500) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+	
+	public String getDecision () {
+		// Add your code here
+		return "pay";
+	}
+	
 }
